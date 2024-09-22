ca92b70dfa731b06f6c3d22848f06c172d3d405a
Miyamura80 <eitomiyamura@gmail.com>
2024-09-22 17:12:29 +0100

    ✨lint

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,9 +16,9 @@ print("Python path:", sys.path)
 # Print the contents of the deps directory
 print("Contents of deps directory:")
 for root, dirs, files in os.walk(deps_path):
-    level = root.replace(deps_path, '').count(os.sep)
-    indent = ' ' * 4 * (level)
+    level = root.replace(deps_path, "").count(os.sep)
+    indent = " " * 4 * (level)
     print(f"{indent}{os.path.basename(root)}/")
-    subindent = ' ' * 4 * (level + 1)
+    subindent = " " * 4 * (level + 1)
     for f in files:
         print(f"{subindent}{f}")
