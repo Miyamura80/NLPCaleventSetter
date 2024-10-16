3fc6dfc303ccf6cec161ec9d2bb09ec311905725
Eito Miyamura <38335479+Miyamura80@users.noreply.github.com>
2024-10-16 12:57:18 +0100

    Update global_config.py

--- global_config/global_config.py
+++ global_config/global_config.py
@@ -1,9 +1,21 @@
 import os
 from typing import Any, Dict
 import yaml
-from dotenv import load_dotenv
+from pathlib import Path
+from dotenv import load_dotenv, dotenv_values
+import warnings
 
-load_dotenv()
+# Get the path to the root directory (one level up from global_config)
+root_dir = Path(__file__).parent.parent
+
+# Load .env file from the root directory
+load_dotenv(dotenv_path=root_dir / ".env")
+
+# Check if .env file has been properly loaded
+env_values = dotenv_values(root_dir / ".env")
+is_local = os.getenv("GITHUB_ACTIONS") != "true"
+if not env_values and is_local:
+    warnings.warn(".env file not found or empty", UserWarning)
 
 
 class DictWrapper:
@@ -16,8 +28,7 @@ class DictWrapper:
 
 
 class Config:
-    OPENAI_API_KEY: str = os.environ.get("OPENAI_API_KEY")
-    HELICONE_API_KEY: str = os.environ.get("HELICONE_API_KEY")
+    _env_keys = ["OPENAI_API_KEY", "HELICONE_API_KEY"]
 
     def __init__(self):
         with open("global_config/global_config.yaml", "r") as file:
@@ -28,8 +39,30 @@ class Config:
             else:
                 setattr(self, key, value)
 
+        # Assert we found all necessary keys
+        for key in self._env_keys:
+            if os.environ.get(key) is None:
+                raise ValueError(f"Environment variable {key} not found")
+            else:
+                setattr(self, key, os.environ.get(key))
+
+        # Figure out runtime environment
+        self.is_local = os.getenv("GITHUB_ACTIONS") != "true"
+        self.running_on = "🖥️  local" if self.is_local else "☁️  CI"
+
     def __getattr__(self, name):
         raise AttributeError(f"'Config' object has no attribute '{name}'")
+
+    def to_dict(self):
+        def unwrap(obj):
+            if isinstance(obj, DictWrapper):
+                return {k: unwrap(v) for k, v in obj.__dict__.items()}
+            elif isinstance(obj, list):
+                return [unwrap(item) for item in obj]
+            else:
+                return obj
+
+        return {k: unwrap(v) for k, v in self.__dict__.items()}
 
 
 # Create a singleton instance
